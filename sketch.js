291cc564edd2922f1a3ef46e62f188c2c2a4f4c4
annettewhitehat <78954185+annettewhitehat@users.noreply.github.com>
2022-01-02 12:53:23 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+
+
+function setup() {
+  createCanvas(800,600);
+  var m1 = createSprite(208,444,15,100);
+  var m2 = createSprite(155,498,120,15);
+  var m3 = createSprite(261,387,120,15);
+  var m4 = createSprite(313,330,15,100);
+  var m5 = createSprite(211,273,220,15);
+  var m6 = createSprite(53,417,100,15);
+  var m7 = createSprite(147,315,15,100);
+  var m8 = createSprite(108,216,15,100);
+  var m9 = createSprite(160,174,100,15);
+  var m10 = createSprite(279,170,15,220);
+  var m11 = createSprite(6,300,15,520);
+  var m12 = createSprite(297,567,590,15);
+  var m13 = createSprite(390,44,590,15);
+  var m14 = createSprite(303,515,15,100);
+  var m15 = createSprite(432,515,15,100);
+  var m16 = createSprite(430,458,140,15);
+  var m17 = createSprite(493,403,15,100);
+  var m18 = createSprite(491,346,150,15);
+  var m19 = createSprite(558,292,15,100);
+  var m20 = createSprite(423,300,15,100);
+  var m21 = createSprite(554,235,100,15);
+  var m22 = createSprite(511,188,15,100);
+
+  
+
+
+}
+
+function draw() {
+  background("blue");  
+  text(mouseX+","+mouseY, mouseX, mouseY);
+  
+  drawSprites();
+}
